6d27ed22b7c64ee9811619177b7fb4b45d12dd9a
maschinetheist <robot999@gmail.com>
2016-05-28 22:16:37 -0400

    Initial checkin of the mugshots.py skeleton for new krokbot module. Ranson's finest now immortolized and tracked as they get arrested.

---
+++ mugshots.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import requests
+import re
+from sqlalchemy import (create_engine, Table, Column, Integer, String, MetaData, ForeignKey, exc)
+from sqlalchemy.sql import (select, exists)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class Jailbird(Base):
+    __tablename__ = 'jailbirds'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    arrestid = Column(String)
+    name = Column(String)
+
+
+'''
+Find details of the jailbird such as name, arrest location, data
+'''
+def find_jailbird_name(url):
+    r = requests.get(url)
+    pattern = re.compile(r"<TITLE>(.+?)  </TITLE>", flags=re.DOTALL)
+    jailbird_name = pattern.findall(r.text)
+    return jailbird_name
+
+def get_arrests():    
+    r = requests.get('http://arre.st/Mugshots/WestVirginia/Arrests/')
+    found = re.findall(r"WV\-[0-9]{10}", r.text)
+    found2 = re.findall(r"http://cdn\.arre\.st/Jails/WVJails\.info/tb\.php\?file=images2/[A-Za-z0-9\-]{1,}\.jpg&size=200", r.text)
+    return found
+
+def start_db():
+    engine = create_engine('sqlite:///mugshots.db')
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+    Base.metadata.reflect(engine)
+    return session
+
+if __name__ == "__main__":
+    session = start_db()
+    found = get_arrests()
+    for arrestid in found:
+        rs = session.query(exists().where(Jailbird.arrestid == arrestid)).scalar()
+        if rs is False:
+            url = "http://arre.st/{arrestid}".format(arrestid=arrestid)
+            jailbird_name = find_jailbird_name(url)
+            print "Adding new Ranson jailbird to the db: {jailbird} {url}".format(jailbird=jailbird_name,url=url)
+            arrestid = str(arrestid)
+            jailbird_name = str(jailbird_name)
+            new_jailbird = Jailbird(arrestid=arrestid, name=jailbird_name)
+            session.add(new_jailbird)
+            session.commit()
