da788797a5feb6ee0aee47d8d40530dcd5ae7aec
Drew Hynes <drew.hynes@gmail.com>
2016-07-30 18:43:21 +0000

    creds.py deleted online with Bitbucket

--- creds.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-tw_consumer_key = 'HwJsftYVShJfpVdn5Id6uS3Ff'
-tw_consumer_secret = 'yuusqzfM1LJT66kqDfa9j0XUpCOQ3KBQa4SK26bywQCZdwDD8L'
-tw_access_token_key = '2409891776-F3f1S4blMbKwNq13Hu3hVYmvtaOyRSwR0dWhPM0'
-tw_access_token_secret = 'HXNL8CcWQRdU7n1F9mvcyT6oAF7uIulAAlFAb1Hl3V47u'
