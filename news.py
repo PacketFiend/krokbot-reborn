1bb836860fe6598b80d7c538e3a1b417aa0080b8
Kenneth Voort <ken@trafficmaster.voort.ca>
2016-11-01 17:42:17 +0000

    Changes to be committed: modified:   news.py
    Added rate limiting and overflow protection, moved more logic into headlineList class

--- news.py
+++ news.py
@@ -29,33 +29,62 @@ news = False
 
 class headlineList:
 
+	maxSearchTerms = 25 # Shared by all instances of this class
+	maxSearchTermLength = 25 # Maximum length of any one searh term - again, shared by all instances
+
 	def __init__(self, name):
 		self.searchList = ['terror','gun','attack','seige','shooting']
 		self.headlines = []
 		self.name = name
-		self.wordlist = ""
+		self.wordlist = ""	# This is just a string built from searchList - so we can print it in a message easily
+		for term in self.searchList:
+			self.wordlist = self.wordlist + " " + term
+		self.rejectedTerms = ""
 	
-	def add_searchTerm(self,searchTerm,nick,channel,bot):
-		if searchTerm in self.searchList:
-			bot.msg(channel,nick + ", " + searchTerm + " is already in the search list.")
-		else:
-			self.searchList.append(searchTerm)
-		self.wordlist = ""
+	def add_searchTerm(self,newTerms,nick,channel,bot):
+
+		# Make sure the list of terms to search for doesn't grow too big
+		newTerms = newTerms.split(" ")	# Shard the passed terms into a list
+		self.rejectedTerms = ""
+		if len(self.searchList) + len(newTerms) > headlineList.maxSearchTerms:
+			bot.msg(channel, nick + ", I can't add that many search terms - the limit is " + str(headlineList.maxSearchTerms))
+		else:	# Add all the terms we received to the search term list
+			for searchTerm in newTerms:
+				if searchTerm in self.searchList:
+					self.rejectedTerms += searchTerm + ": already present; "
+				elif len(searchTerm) > headlineList.maxSearchTermLength:
+					self.rejectedTerms += searchTerm + ": >" + str(headlineList.maxSearchTermLength) + " chars; "
+				else:
+					self.searchList.append(searchTerm)
+
+		# Construct a string conatining all the search terms
+		self.wordlist = ""	
 		for term in self.searchList:
 			self.wordlist = self.wordlist + " " + term
 
+		# Clean up the strings we constructed
+		self.wordlist = self.wordlist.rstrip()
+		self.rejectedTerms = self.rejectedTerms.rstrip('; ');
 		print self.searchList
 	
-	def remove_searchTerm(self,searchTerm,nick,channel,bot):
-		if searchTerm in self.searchList:
-			self.searchList.remove(searchTerm)
-		else:
-			bot.msg(channel,nick + ", " + searchTerm + " is not in the search list, so I can't remove it.")
+	def remove_searchTerm(self,terms2Remove,nick,channel,bot):
 
+		terms2Remove = terms2Remove.split(" ")	# Shard the passed string of terms into a list
+		self.rejectedTerms = ""
+		for searchTerm in terms2Remove:		# Remove the list of terms from the search list
+			if searchTerm in self.searchList:
+				self.searchList.remove(searchTerm)
+			else:
+				self.rejectedTerms += searchTerm + " "
+
+		# Construct a string containing all the search terms
 		self.wordlist = ""
 		for term in self.searchList:
 			self.wordlist = self.wordlist + " " + term
 
+		# Clean up the strings we constructed
+		self.wordlist = self.wordlist.rstrip()
+		self.rejectedTerms = self.rejectedTerms.rstrip()
 		print self.searchList
 	
 	# Find any headlines with matching keywords. Search is case sensitive, so we convert to lowercase before searching.
@@ -73,33 +102,53 @@ class headlineList:
 
 defaultHeadlines = headlineList('default')
 
+@module.rate(30)
 @module.commands('addtoheadlines')
 def addtoheadlines(bot, trigger):
+	"""Adds a new search term to the headline search. Multiple terms can be added at once."""
+	if news == False: return
+
 	imp = trigger.group(2)
-	shard = imp.split(" ")
-	for term in shard:
-		defaultHeadlines.add_searchTerm(term,trigger.nick,trigger.sender,bot)
+	defaultHeadlines.add_searchTerm(imp,trigger.nick,trigger.sender,bot)
 
-	bot.msg(trigger.sender, trigger.nick + ", now searchnig for these words: " + defaultHeadlines.wordlist)
+	bot.msg(trigger.sender, trigger.nick + ", now searching for these words: " + defaultHeadlines.wordlist)
+	# If we rejected any search terms, let someone know
+	if defaultHeadlines.rejectedTerms:
+		bot.msg(trigger.sender, trigger.nick + ", could not add these terms: " + defaultHeadlines.rejectedTerms)
 
+@module.rate(30)
 @module.commands('removefromheadlines')
 def removefromheadlines(bot, trigger):
-	imp = trigger.group(2)
-	shard = imp.split(" ")
-	for term in shard:
-		defaultHeadlines.remove_searchTerm(term,trigger.nick,trigger.sender,bot)
+	""" Removes a term from the headline search. Multiple terms may be removed at once."""
+	if news == False: return
 
-	bot.msg(trigger.sender, trigger.nick + ", now searchnig for these words: " + defaultHeadlines.wordlist)
+	imp = trigger.group(2)
+	defaultHeadlines.remove_searchTerm(imp,trigger.nick,trigger.sender,bot)
+
+	# If we couldn't remove any terms, let someone know
+	if defaultHeadlines.rejectedTerms:
+		bot.msg(trigger.sender, trigger.nick + ", you suck. These terms are not in the list: " + defaultHeadlines.rejectedTerms)
+
+	bot.msg(trigger.sender, trigger.nick + ", now searching for these words: " + defaultHeadlines.wordlist)
 
 # Function to fetch the rss feed and return the parsed RSS
 def parseRSS( rss_url ):
-    return feedparser.parse( rss_url )
+    try:
+	parsedRSS = feedparser.parse( rss_url )
+    except:
+	print "Unhandled excpetion: ",sys.exc_info()[0]
+	raise
+    return parsedRSS
 
 # Function grabs the rss feed headlines (titles) and returns them as a list
 def getHeadlines( rss_url ):
     headlines = []
 
-    feed = parseRSS( rss_url )
+    try:
+	feed = parseRSS( rss_url )
+    except:
+	print "Unhandled exception: ",sys.exec_info()[0]
+	raise
     for newsitem in feed['items']:
 	newsitem['title'] += " - " + tinyurl.create_one(newsitem['link'])
         headlines.append(newsitem['title'])
@@ -141,8 +190,10 @@ def news_disable(bot, trigger):
 	bot.say(message)
 
 #@module.interval(120)
+@module.rate(30)
 @module.commands('news_announce')
 def news_announce(bot, trigger):
+	"""Search the provided news feeds for news matching the current search term list. Results are printed to the channel with tinuyrl links."""
 	# when using module.interval() you dont need to pass trigger to the function, just bot
 	#defaultHeadlines = headlineList('default')
 	global news
@@ -152,18 +203,20 @@ def news_announce(bot, trigger):
 
 		# List of RSS feeds that we will fetch and combine
 		newsurls = {
-		    'googlenews':       'http://news.google.com/news?cf=all&hl=en&pz=1&ned=us&output=rss'
+		    'googlenews':       'https://news.google.com/news?cf=all&hl=en&pz=1&ned=us&num=15&output=rss'
 		}
 
 		# Iterate over the feed urls
 		for key,url in newsurls.items():
 		    # Call getHeadlines() and combine the returned headlines with allheadlines
 		    allheadlines.extend( getHeadlines( url ) )
+		print "NEWS: "
 		print allheadlines
+		print "*****"
 		defaultHeadlines.headlines = allheadlines
 		things = defaultHeadlines.find_hits()
 		for t in things:
-			bot.msg("#safespace",t,1)	
+			bot.msg(trigger.sender,t,1)	
 		#message = "The Krokbot News System: keeping you informed of up-to-the-minute jihad jihad"
 		#bot.msg("#lounge",message, 1)
 
