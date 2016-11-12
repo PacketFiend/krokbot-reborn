6525ac1c78a72efee3c26a5d1e7c45645029638d
Kenneth Voort <kenneth@voort.ca>
2016-11-12 15:52:31 +0000

    modified:   antiDDoS.py - Cleaned up some code, removed some debug messages, and now sets limit on kill/quit/kick and not just part/join

--- antiDDoS.py
+++ antiDDoS.py
@@ -8,7 +8,6 @@
 
 from sopel import module, tools, bot
 from sopel.tools.target import User, Channel
-import random	# We might decide to only sample 1 in 10 messages or something
 from random import randint
 import sys
 import threading
@@ -25,10 +24,7 @@ def channel_limit(bot, trigger):
 	newLimit = trigger.group(2)
 	bot.msg(trigger.sender, trigger.nick + ", setting channel limit to " + newLimit)
 
-	#try:
 	bot.write(['MODE', trigger.sender , "+l" , newLimit])
-	#except:
-	#bot.msg(trigger.sender, trigger.nick + " , unhandled exception occured :`(")
 
 
 @module.commands('show_users')
@@ -41,52 +37,31 @@ def show_users(bot, trigger):
 
 
 @module.event('PART')
-@module.unblockable
-@module.rule('.*')
-def userPart(bot, trigger):
-	# Sets a new channel user limit every time a user parts a channel in which the bot is at least a halfop
-	# The new limit will be equal to the number of users in the channel, plus a margin which can be changed in setLimitMargin
-
-	global limitMargin
-	global lockedChannels
-	if bot.privileges[trigger.sender][bot.nick] < module.HALFOP:
-		print "Can't set the channel limit, as I'm not at leaft a halfop in " + trigger.sender
-		return
-	if trigger.sender in lockedChannels:
-		print trigger.sender + " has a locked down channel limit. We're probably being DDoS'ed"
-		bot.msg(trigger.sender, "Not changing the channel limit. We are locked down for now.")
-		return
-
-	# Calculate the new channel limit to be the number of users plus the margin we specified, tracked as limitMargin
-	newLimit = len(bot.channels[trigger.sender].users) + limitMargin
-
-	bot.msg(trigger.sender, "Setting channel limit to " + str(newLimit))
-	# Set the new channel limit
-	bot.write(['MODE', trigger.sender , "+l" , str(newLimit)])
-
 @module.event('JOIN')
+@module.event('KICK')
+@module.event('QUIT')
+@module.event('KILL')
+@module.priority('high')
 @module.unblockable
 @module.rule('.*')
 def userJoin(bot, trigger):
-	# Sets a new channel user limit every time a user joins a channel in which the bot is at least a halfop
+	# Sets a new channel user limit on specific channel or server events in channels where the bot is at least a halfop
 	# The new limit will be equal to the number of users in the channel, plus a margin which can be changed in setLimitMargin
+	# Is called on every part, join, kick, quit, and kill evemt
 
 	global limitMargin
 	global lockedChannels
-	if bot.privileges[trigger.sender][bot.nick] < module.HALFOP:
-		print "Can't set the channel limit, as I'm not at leaft a halfop in " + trigger.sender
-		return
-	if trigger.sender in lockedChannels:
-		print trigger.sender + " has a locked down channel limit. We're probably being DDoS'ed"
-		bot.msg(trigger.sender, "Not changing the channel limit. We are locked down for now.")
-		return
 
-	# Calculate the new channel limit to be the number of users plus the margin we specified, tracked as limitMargin
-	newLimit = len(bot.channels[trigger.sender].users) + limitMargin
-
-	bot.msg(trigger.sender, "Setting channel limit to " + str(newLimit))
-	# Set the new channel limit
-	bot.write(['MODE', trigger.sender , "+l" , str(newLimit)])
+	# If this was a JOIN, PART, or KICK event , we know which channel it occured in.
+	# On KILL and QUIT events, we don't so we reset the channel limit on /every/ channel.
+	if trigger.event == 'KICK' or trigger.event == 'JOIN' or trigger.event == 'PART':
+		print "PART/KICK/JOIN event triggered!"
+		setChannelLimit(bot, trigger.sender)
+	else:
+		# We don't know which channel this was sent from, so we'll just cycle through every channel we're in...
+		print "KILL or QUIT event triggered!"
+		for channel in bot.channels:
+			setChannelLimit(bot, channel)
 
 @module.require_admin
 @module.commands('set_limit_margin')
@@ -101,6 +76,7 @@ def setLimitMargin(bot, trigger):
 @module.require_admin
 @module.commands('lock_channel_limit')
 @module.unblockable
+@module.priority('high')
 def lockChannelLimit(bot, trigger):
 	'''Locks down the channel limit it is called in. This is to be used in the case of DDoS attacks'''
 
@@ -126,3 +102,23 @@ def unlockChannelLimit(bot, trigger):
 		userJoin(bot, trigger)
 	else:
 		bot.msg(trigger.sender, trigger.nick + ", this channel limit is already unlocked, dumbass.")
+
+def setChannelLimit(bot, channel):
+	# Sets a new channel limit on the given channel based on the number of users in the channel and an operator-defined margin
+	global limitMargin
+	global lockedChannels
+
+	if bot.privileges[channel][bot.nick] < module.HALFOP:
+		print "Can't set the channel limit, as I'm not at leaft a halfop in " + channel
+		return
+	if channel in lockedChannels:
+		print channel + " has a locked down channel limit. We're probably being DDoS'ed"
+		bot.msg(channel, "Not changing the channel limit. We are locked down for now.")
+		return
+	# Calculate the new channel limit to be the number of users plus the margin we specified, tracked as limitMargin
+	newLimit = len(bot.channels[channel].users) + limitMargin
+	bot.msg(channel, "Setting channel limit to " + str(newLimit))
+
+	# Set the new channel limit
+	bot.write(['MODE', channel , "+l" , str(newLimit)])
+
