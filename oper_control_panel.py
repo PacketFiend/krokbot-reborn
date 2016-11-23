326dfb49693c92cb62184be799977f05a4631f51
Mike <mike@project6209.com>
2016-11-23 12:32:52 -0600

    Adding oper control panel

---
+++ oper_control_panel.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+#
+# Name:    oper.py
+# Author:  ekim
+# Date:    Nov 23rd, 2016
+# Summary: OPER mode command control panel
+#
+
+from __future__ import print_function
+from sopel import module, tools, config
+from sopel.tools.target import User, Channel
+from sopel.tools import Identifier, iteritems, events
+
+'''
+Define some memory dicts/lists for keeping track of users who become OPs.
+'''
+def setup(bot):
+    bot.memory['opers'] = {}
+
+#@module.require_privmsg
+@module.require_admin
+@module.commands('opme')
+@module.priority('low')
+#@module.rate('300')
+def opme(bot, trigger):
+    nickname = trigger.nick
+    channel = trigger.sender
+
+    if trigger.admin:
+        if bot.memory['opers'].get(nickname, None) == None:
+            mode = "+o"
+            bot.write(['MODE ', channel, mode, nickname])
+            bot.memory['opers'][nickname] = 1
+            print(bot.memory['opers'])
+        elif bot.memory['opers'].get(nickname, None) == 1:
+            mode = "-o"
+            bot.write(['MODE ', channel, mode, nickname])
+            del bot.memory['opers'][nickname]
+            print(bot.memory['opers'])
+    else:
+        bot.reply("No ops for you chamo, your mama don't love you")
+
+@module.require_admin
+@module.commands('op')
+@module.priority('low')
+#@module.rate('300')
+def op_user(bot, trigger):
+    if trigger.admin:
+        nickname = trigger.group(2)
+        channel = trigger.sender
+
+        if bot.memory['opers'].get(nickname, None) == None:
+            mode = "+o"
+            bot.write(['MODE ', channel, mode, nickname])
+            bot.memory['opers'][nickname] = 1
+            print(bot.memory['opers'])
+        elif bot.memory['opers'].get(nickname, None) == 1:
+            mode = "-o"
+            bot.write(['MODE ', channel, mode, nickname])
+            del bot.memory['opers'][nickname]
+            print(bot.memory['opers'])
+    else:
+        bot.reply("No ops for that chamo, go chop bongs")
