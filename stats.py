277badc7115b548a7631d80b2cddd37deeb33c07
Mike <mike@project6209.com>
2016-12-16 21:22:31 -0600

    Fixed decoding problems

--- stats.py
+++ stats.py
@@ -69,7 +69,7 @@ def setup(bot):
     session.close()
     for result in results:
         channel = result[1]
-        channel = channel.encode('ascii')
+        channel = channel.decode('utf-8')
         bot.memory['word_counts'] = {}
         bot.memory['word_counts'][channel] = {}
 
@@ -85,7 +85,7 @@ def insert_top_action(bot, trigger):
     if trigger.match and 'sopel' not in trigger.nick:
         nickname = trigger.nick
         channel = trigger.sender
-        channel = channel.encode('ascii')
+        channel = channel.decode('utf-8')
         actions = []
 
         if 'lather' in trigger.group(0):
@@ -135,7 +135,7 @@ database table we'll be querying.
 @module.commands('toplather', 'toplure', 'topbait', 'words')
 def get_top_stats(bot, trigger):
     channel = trigger.sender
-    channel = channel.encode('ascii')
+    channel = channel.decode('utf-8')
 
     if 'lather' in trigger.group(1):
         reply = "Top Lather Action"
@@ -155,7 +155,7 @@ def get_top_stats(bot, trigger):
 
     session.close()
 
-    stats = {k.encode('ascii'): v for k, v in top_stats.items()}
+    stats = {k.decode('utf-8'): v for k, v in top_stats.items()}
     bot.msg(channel, reply + ": " + str(stats))
 
 '''
@@ -180,9 +180,9 @@ nickname, count
 @module.rule(r'.*')
 def words_stats(bot, trigger):
     nickname = trigger.user
-    nickname = nickname.encode('ascii')
+    nickname = nickname.decode('utf-8')
     channel = trigger.sender
-    channel = channel.encode('ascii')
+    channel = channel.decode('utf-8')
     line = trigger.args[1:]
     line =  map(str, line)
     #line = line.encode('ascii')
