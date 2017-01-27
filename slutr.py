ce72c7c28e19e0b485aa8511a7b270d65a7d80a5
Mike <mike@project6209.com>
2017-01-26 19:01:49 -0600

    Optimized slutr.py to remove extra code; moved all the command calls into one def function

--- slutr.py
+++ slutr.py
@@ -14,10 +14,10 @@ import praw
 # Define some memory dicts/lists for keeping track of pics that were already used
 def setup(bot):
     bot.memory["used_pics"] = {}
-    bot.memory["used_pics"]['tits'] = []
-    bot.memory["used_pics"]['clams'] = []
-    bot.memory["used_pics"]['sideboobs'] = []
-    bot.memory["used_pics"]['sharpies'] = []
+    bot.memory["used_pics"]['tittypic'] = []
+    bot.memory["used_pics"]['clamshot'] = []
+    bot.memory["used_pics"]['sideboob'] = []
+    bot.memory["used_pics"]['sharpie'] = []
 
 # slutr extensions
 # Reddit titty pic poster
@@ -31,11 +31,11 @@ def sluttosphere_setup(bot):
     global slut_links_tits
     global slut_links_clams
     global slut_links_sideboobs
-    global slut_links_buttsharpies
-    slut_links_tits = []
-    slut_links_clams = []
-    slut_links_sideboobs = []
-    slut_links_buttsharpies = []
+    global slut_links_sharpie
+    slut_links_tits = {}
+    slut_links_clams = {}
+    slut_links_sideboobs = {}
+    slut_links_sharpie = {}
 
     for sub in titty_subreddits:
         r_posts = r.get_subreddit(sub).get_new(limit=15)
@@ -53,102 +53,59 @@ def sluttosphere_setup(bot):
 
     for sub in sharpies_subreddits:
         r_posts = r.get_subreddit(sub).get_new(limit=15)
-        slut_links_buttsharpies = {s.id: [s.title, s.url] for s in r_posts}
+        slut_links_sharpie = {s.id: [s.title, s.url] for s in r_posts}
 
     print(sluttosphere_setup.__name__ + " - Grabbed latest batch of slut pics")
 
-    return (slut_links_tits, slut_links_clams, slut_links_sideboobs, slut_links_buttsharpies)
+    return (slut_links_tits, slut_links_clams, slut_links_sideboobs, slut_links_sharpie)
 
 @module.rate(20)
-@module.commands('tittypic')
-def sluttosphere_tits(bot, trigger):
+@module.commands(r'tittypic\b|clamshot\b|sideboob\b|sharpie\b')
+def sluttosphere_get_pic(bot, trigger):
     try:
+        if 'tittypic' in trigger.group(0):
+            pic_type = 'tittypic'
+            pics = slut_links_tits
+        elif 'clamshot' in trigger.group(0):
+            pic_type = 'clamshot'
+            pics = slut_links_clams
+        elif 'sideboob' in trigger.group(0):
+            pic_type = 'sideboob'
+            pics = slut_links_sideboobs
+        elif 'sharpie' in trigger.group(0):
+            pic_type = 'sharpie'
+            pics = slut_links_sharpie
+        else:
+            pics = "can't find the dict"
+
         # first we pick a random pic, then we check if it is in the memory and if
         # the memory is full for each type of pics.
         while True:
-            pic = random.choice(slut_links_tits.keys())
-            if pic in bot.memory['used_pics']['tits']:
+            print(pics)
+            pic = random.choice(pics.keys())
+            if pic in bot.memory['used_pics'][pic_type]:
                 pass
-            elif len(bot.memory['used_pics']['tits']) >= 15:
+            elif len(bot.memory['used_pics'][pic_type]) >= 15:
                 bot.say("Hold on, the sluts are coming!")
                 break
             else:
-                rand_submission = "NSFW - [" + slut_links_tits[pic][0] + "] " + slut_links_tits[pic][1]
-                print(sluttosphere_clams.__name__ + " - " + rand_submission)
+                rand_submission = "NSFW - [" + pics[pic][0] + "] " + pics[pic][1]
+                print(sluttosphere_get_pic.__name__ + " - " + rand_submission)
                 bot.say(rand_submission)
-                bot.memory['used_pics']['tits'].append(pic)
+                bot.memory['used_pics'][pic_type].append(pic)
                 break
     except (KeyError, IndexError, NameError):
-        errmsg = "Got no sluts yet"
-        bot.say(errmsg)
-        dummy_arg = None
-        sluttosphere_setup(dummy_arg)
-
-@module.rate(20)
-@module.commands('clamshot')
-def sluttosphere_clams(bot, trigger):
-    try:
-        while True:
-            pic = random.choice(slut_links_clams.keys())
-            if pic in bot.memory['used_pics']['clams']:
-                pass
-            elif len(bot.memory['used_pics']['clams']) >= 15:
-                bot.say("Hold on, the sluts are coming!")
-                break
-            else:
-                rand_submission = "NSFW - [" + slut_links_clams[pic][0] + "] " + slut_links_clams[pic][1]
-                print(sluttosphere_clams.__name__ + " - " + rand_submission)
-                bot.say(rand_submission)
-                bot.memory['used_pics']['clams'].append(pic)
-                break
-    except (KeyError, IndexError, NameError):
-        errmsg = "Got no pink tacos yet"
-        bot.say(errmsg)
-        dummy_arg = None
-        sluttosphere_setup(dummy_arg)
-
-@module.rate(20)
-@module.commands('sideboob')
-def sluttosphere_sideboob(bot, trigger):
-    try:
-        while True:
-            pic = random.choice(slut_links_sideboobs.keys())
-            if pic in bot.memory['used_pics']['sideboobs']:
-                pass
-            elif len(bot.memory['used_pics']['sideboobs']) >= 15:
-                bot.say("Hold on, the sluts are coming!")
-                break
-            else:
-                rand_submission = "NSFW - [" + slut_links_sideboobs[pic][0] + "] " + slut_links_sideboobs[pic][1]
-                print(sluttosphere_clams.__name__ + " - " + rand_submission)
-                bot.say(rand_submission)
-                bot.memory['used_pics']['sideboobs'].append(pic)
-                break
-    except (KeyError, IndexError, NameError):
-        errmsg = "Got no poppin' side boobs yet"
-        bot.say(errmsg)
-        dummy_arg = None
-        sluttosphere_setup(dummy_arg)
-
-@module.rate(20)
-@module.commands('sharpie')
-def sluttosphere_sharpie(bot, trigger):
-    try:
-        while True:
-            pic = random.choice(slut_links_sharpies.keys())
-            if pic in bot.memory['used_pics']['sharpies']:
-                pass
-            elif len(bot.memory['used_pics']['sharpies']) >= 15:
-                bot.say("Hold on, the sluts are coming!")
-                break
-            else:
-                rand_submission = "NSFW - [" + slut_links_sharpies[pic][0] + "] " + slut_links_sharpies[pic][1]
-                print(sluttosphere_clams.__name__ + " - " + rand_submission)
-                bot.say(rand_submission)
-                bot.memory['used_pics']['sharpies'].append(pic)
-                break
-    except (KeyError, IndexError, NameError):
-        errmsg = "Let's see how many sharpies I can fit up my asshole"
+        if 'tittypic' in trigger.group(0):
+            errmsg = "Got no sluts yet"
+        elif 'clamshot' in trigger.group(0):
+            errmsg = "Got no pink tacos yet"
+        elif 'sideboob' in trigger.group(0):
+            errmsg = "Got no poppin' side boobs yet"
+        elif 'sharpie' in trigger.group(0):
+            errmsg = "Let's see how many sharpies I can fit up my asshole"
+        else:
+            print(trigger.group(0))
+            errmsg = "Hold on, the sluts are coming!"
         bot.say(errmsg)
         dummy_arg = None
         sluttosphere_setup(dummy_arg)
